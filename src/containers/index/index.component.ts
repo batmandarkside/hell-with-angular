a76a80c74bd784248c3893b642d143c0acb43037
Dmitry Kuznetsov <dmitry.kuznetsov>
2018-02-28 20:09:33 +0300

    start angular app

--- src/containers/index/index.component.ts
+++ src/containers/index/index.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-index-container',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class IndexPageComponent {
+  containerName = 'Index container';
 }
--- src/containers/index/index.module.ts
+++ src/containers/index/index.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { IndexPageComponent } from './index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    IndexPageComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [IndexPageComponent]
 })
-export class AppModule { }
+export class IndexModule { }
